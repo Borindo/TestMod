c8d87d5ea0bc91eab1952b7930fd5e607d8399b8
Borindeus <kolyada.boris@gmail.com>
2018-04-09 14:22:33 +0300

    Исправлено mcmod.info

---
+++ src/main/java/com/borindeus/testmod/TestMod.java
@@ -0,0 +1,9 @@
+package com.borindeus.testmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "testmod", name = "Test Mod", version = "@VERSION@", dependencies = "required-after:hammercore")
+public class TestMod
+{
+	
+}
